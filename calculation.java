9afe8e5c8ff1fa8c996ed635fbb7bffc5a4b9643
Kiranmaruti11 <113971744+Kiranmaruti11@users.noreply.github.com>
2022-10-20 20:24:55 -0700

    Create calculation.java

---
+++ calculation.java
@@ -0,0 +1,19 @@
+package calculator;
+public class calculation {
+    public int adder(int a,int b){
+        return a+b;
+    }
+    public int subtract(int a,int b){
+        return a-b;
+
+    }
+    public int division(int a,int b){
+        return a/b;
+
+    }
+    public int multiplication(int a,int b){
+        return a*b;
+    }
+
+
+}
